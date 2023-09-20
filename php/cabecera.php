1d20f196d0d1008e6dbbcd76d8da963a0318c54c
Angel Salom <angel@visionwin.com>
2023-09-20 13:26:18 +0200

    Quito base href de cabecera.php

--- php/cabecera.php
+++ php/cabecera.php
@@ -18,7 +18,6 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <base href="https://www.visionwin.com/" />
 
     <title itemprop="name"><?=$pagetitle?></title>
     <meta name="description" itemprop="description" content="<?=$pagedescription?>" />
