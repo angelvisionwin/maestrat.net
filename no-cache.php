15d269dba2366df5ba6a8a0d8a186eec37cc4527
Angel Salom <angel@visionwin.com>
2023-09-21 09:55:05 +0200

    Ajuste en no-cache.php

--- no-cache.php
+++ no-cache.php
@@ -1,4 +1,7 @@
 <?php
-    header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-    header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
+    header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+    header("Cache-Control: no-store, no-cache, must-revalidate");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
 ?>
